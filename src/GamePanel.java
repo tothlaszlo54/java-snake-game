23637b8f2f8cbdc6c5266e6f5ee9a99575355c15
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-05-03 15:05:29 +0200

    The paintComponent method have been completed.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -42,7 +42,8 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        draw(g);
     }
 
     public void draw(Graphics g) {
